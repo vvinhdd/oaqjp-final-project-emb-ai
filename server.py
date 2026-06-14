835803d7132c50ed40e3c45522802c2bbda71d1d
vvinhdd <ddvinh13@gmail.com>
2026-06-14 14:21:42 -0400

    Task 6: Web deployment of the application using Flask

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def emot_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    system_response = "For the given statement, the system response is "
+
+    dynamic_emotions = [(k, v) for k, v in response.items() if k != 'dominant_emotion']
+    for i, (key, value) in enumerate(dynamic_emotions):
+        score = str(value)
+        if i == len(dynamic_emotions) - 1:
+            system_response += "and '" + key + "': " + score
+        else:
+            system_response += "'" + key + "': " + score + ", "
+
+    system_response += ". The dominant emotion is " + response['dominant_emotion'] + "." 
+    return system_response
+
