212900fffd8e27dd0a4c2b73b52e2041140886ff
vvinhdd <ddvinh13@gmail.com>
2026-06-14 14:50:52 -0400

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
+"""
+Flask server application for Emotion Detection.
+Provides routes to render the user interface and analyze emotions from text.
+"""
 from flask import Flask, render_template, request
-
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+@APP.route("/")
 def render_index_page():
+    """
+    Renders the main index.html page for the user interface.
+    """
     return render_template('index.html')
 
-@app.route("/emotionDetector")
+@APP.route("/emotionDetector")
 def emot_detector():
+    """
+    Analyzes the input text using emotion_detector and returns a formatted string.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     system_response = "For the given statement, the system response is "
@@ -25,6 +34,9 @@ def emot_detector():
         else:
             system_response += "'" + key + "': " + score + ", "
 
-    system_response += ". The dominant emotion is " + response['dominant_emotion'] + "." 
+    system_response += ". The dominant emotion is " + response['dominant_emotion'] + "."
     return system_response
 
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=5000)
+    
